6d6028df6b2f4fe391f994a52a33e09ceb099bec
hama0115 <oh.akibee1231@gmail.com>
2025-05-30 07:50:45 +0900

    ヘッダーのボタンにホバー時の挙動をつけた。また、PC版ではSNSを非表示に

--- header.php
+++ header.php
@@ -11,17 +11,6 @@
             <div class="header__logo">
                 <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" /></a>
             </div>
-            <!-- <nav class="header__nav">
-                <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-                </ul>
-            </nav> -->
-            <!-- <div class="header__search">
-                <input type="text" placeholder="Search..." />
-            </div> -->
             <input type="checkbox" class="menu-btn" id="menu-btn">
             <label for="menu-btn" class="menu-icon"><span class="navicon"></span></label>
 
@@ -30,8 +19,8 @@
                     <li><a href="/"><p class="txt-18">TOP</p></a></li>
                     <li><a href="<?php echo get_permalink( get_page_by_path('contact') ); ?>"><p class="txt-18">お問い合わせ</p></a></li>
                     <li><a href="<?php echo get_permalink( get_page_by_path('form') ); ?>"><p class="txt-18">WEB予約</p></a></li>
-                    <li><a href="#"><i class="fab fa-instagram fa-4x"></i></a></li>
-                    <li><a href="#"><i class="fab fa-line fa-4x"></i></a></li>
+                    <li class="sns-item"><a href="#"><i class="fab fa-instagram fa-4x"></i></a></li>
+                    <li class="sns-item"><a href="#"><i class="fab fa-line fa-4x"></i></a></li>
                     <li class="pc"><p class="txt-20">Tel. 090-9227-2013</p></li>
                     <li class="tb"><a href="tel:00000000000"><p class="txt-20"><i class="fas fa-phone fa-3x"></i>  090-9227-2013</p></a></li>
                 </ul>
