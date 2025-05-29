bf5dbd1a6e65315d07e2f44082807bd27af7fbc9
hama0115 <oh.akibee1231@gmail.com>
2025-05-30 07:11:26 +0900

    コピーライトを訂正

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
    </main>
     <footer>
-        <p class="txt-14 txt-white">Copyright @ Chiropractor KitaShuto All Rights Reserved.</p>
+        <p class="txt-14 txt-white">©︎ 2025 Chiropractic-donguri All rights reserved.</p>
     </footer>
     <?php wp_footer(); ?>
 </body>
