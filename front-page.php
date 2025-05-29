e3eaa65bccdab2f4680779cad2504dbc1ef06263
hama0115 <oh.akibee1231@gmail.com>
2025-05-30 07:07:11 +0900

    AOSのアニメーションを一部の強調したい部分にしぼった

--- front-page.php
+++ front-page.php
@@ -25,7 +25,7 @@
         </div>
         <div id="about__area" class="wrap">
             <section class="inner flex-start">
-                <div class="txt-area" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="txt-area">
                     <div class="title">
                         <p class="txt-14 en">About</p>
                         <h2 class="txt-24">カイロプラクティックとは？</h2>
@@ -47,18 +47,18 @@
                             根本からの改善を目指して、一緒に取り組んでいきましょう。</p>
                     </div>
                 </div>
-                <div class="pht-area" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="pht-area">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/about.png" alt="">
                 </div>
             </section>
         </div>
         <div id="time__area">
             <section class="inner">
-                <div class="title txt-white" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="title txt-white">
                     <p class="txt-14 en">Timeline</p>
                     <h2 class="txt-24">施術の流れ</h2>
                 </div>
-                <p class="txt-16 txt-center txt-white" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">問診から施術まで１時間程度となります。</p>
+                <p class="txt-16 txt-center txt-white">問診から施術まで１時間程度となります。</p>
                 <div class="content flex-start">
                     <div class="item" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
                         <p class="txt-24">1</p>
@@ -84,46 +84,46 @@
         </div>
         <div id="price__area">
             <section class="inner">
-                <div class="title" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="title">
                     <p class="txt-14 en">Price</p>
                     <h2 class="txt-24">料金案内</h2>
                 </div>
-                <div class="price-box" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="price-box">
                     <h3 class="txt-20">基本料金</h3>
                     <div class="txt">
                         <p class="txt-24">施術時間：60分</p>
                         <p class="txt-48"><span class="en-a">3,500</span><span class="txt-20">円</span></p>
                     </div>
                 </div>
-                <div class="price-box" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="price-box">
                     <h3 class="txt-20">出張料金</h3>
                     <div class="txt">
                         <p class="txt-16">お客様のご自宅などへの出張施術を希望される場合</p>
                         <p class="txt-48"><span class="txt-24">基本料金 + </span><span class="en-a"> 500</span><span class="txt-20">円</span></p>
                     </div>
-                    <p class="fukidashi txt-16">
+                    <p class="fukidashi txt-16" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
                         <span>伊勢市、その周辺への出張が可能です!!<br>ご自宅で施術を受けることができます。<br>ご予約の際、出張を希望する旨をお申し付けください。</span>
                     </p>
                 </div>
-                <p class="txt-20 center" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true"><span>初診の場合、<span class="bold">最大で<span class="txt-24">90</span>分</span>ほどお時間をいただき<br class="pc">施術させていただくことがあります。</span></p>
+                <p class="txt-20 center"><span>初診の場合、<span class="bold">最大で<span class="txt-24">90</span>分</span>ほどお時間をいただき<br class="pc">施術させていただくことがあります。</span></p>
                 <div class="btn" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
                     <a href="<?php echo get_permalink( get_page_by_path('form') ); ?>"><span class="txt-20 txt-white">WEB予約はこちら</span></a>
                     <a href="<?php echo get_permalink( get_page_by_path('contact') ); ?>"><span class="txt-20 txt-white">お問い合わせはこちら</span></a>
                 </div>
-                <div class="pht-area" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="pht-area">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/price.png" alt="">
                 </div>
             </section>
         </div>
         <div id="profile__area">
             <section class="inner">
-                <div class="title txt-white" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="title txt-white">
                     <p class="txt-14 en">Greeting</p>
                     <h2 class="txt-24">ごあいさつ</h2>
                 </div>
-                <div class="flex" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="flex">
                     <div class="pht">
-                        <div class="profile-img">
+                        <div class="profile-img" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
                           <?php //フロントページのカスタムフィールドで設定した画像を表示
                           $profile = get_field('profile-img');
                           if ($profile): ?>
@@ -134,7 +134,7 @@
                         </div>
                         <p class="txt-16 txt-white">カイロプラクティック<br class="pc">どんぐり 喜多</p>
                     </div>
-                    <div class="txt txt-white">
+                    <div class="txt txt-white" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
                         <p class="txt-16">ホームページをご覧いただき、誠にありがとうございます。</p>
                         <p class="txt-16">「身体本来の力を引き出す施術」を大切にし、お一人おひとりの症状や生活スタイルに寄り添ったカイロプラクティック施術を提供しております。肩こりや腰痛、姿勢のゆがみなど、現代人が抱えるさまざまな不調は、身体のバランスの乱れからくることが少なくありません。<br>
 だからこそ、ただ一時的な痛みを和らげるのではなく、根本的な原因に向き合い、丁寧なカウンセリングと施術を通じて、健康的な身体づくりをサポートしていきたいと考えております。<br></p>
@@ -146,13 +146,13 @@
         </div>
         <div id="calendar__area">
             <section class="inner">
-                <div class="title" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="title">
                     <p class="txt-14 en">Calendar</p>
                     <h2 class="txt-24">カレンダー</h2>
                 </div>
-                <p class="txt-16" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">ご予約はお電話、もしくは予約フォームより受け付けております。ご都合のよい日時をお選びください。</p>
+                <p class="txt-16">ご予約はお電話、もしくは予約フォームより受け付けております。ご都合のよい日時をお選びください。</p>
                 <div class="cntnt">
-                    <table class="table" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                    <table class="table">
                         <tr>
                             <th class="txt-16">診療時間</th>
                             <td class="txt-16">月</td>
@@ -174,8 +174,8 @@
                             <td class="txt-16">○</td>
                         </tr>
                     </table>
-                    <p class="txt-14" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">休診日：不定休</p>
-                    <p class="txt-14" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">営業カレンダーは目安です。施術者の都合により、休みをいただく場合がございますのでご了承ください。また、時間に関しましては柔軟に対応可能です。「お問い合わせ」からご連絡ください。<br>当日予約はお電話で、明日以降はWEB予約が可能です。</p>
+                    <p class="txt-14">休診日：不定休</p>
+                    <p class="txt-14">営業カレンダーは目安です。施術者の都合により、休みをいただく場合がございますのでご了承ください。また、時間に関しましては柔軟に対応可能です。「お問い合わせ」からご連絡ください。<br>当日予約はお電話で、明日以降はWEB予約が可能です。</p>
                 </div>
                 <div class="btn" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
                     <a href="<?php echo get_permalink( get_page_by_path('form') ); ?>"><span class="txt-20 txt-white">WEB予約はこちら</span></a>
@@ -188,11 +188,11 @@
         </div>
         <div id="map__area">
             <section class="inner">
-                <div class="title txt-white" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="title txt-white">
                     <p class="txt-14 en">Access & Map</p>
                     <h2 class="txt-24">アクセス＆マップ</h2>
                 </div>
-                <div class="flex" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+                <div class="flex">
                     <div class="pht-area">
                         <img src="<?php echo get_template_directory_uri(); ?>/img/map.png" alt="出張マップ">
                     </div>
