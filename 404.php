34ac59c31b8f624e4082c3453582d0f70c0e5571
hama0115 <oh.akibee1231@gmail.com>
2025-06-08 18:18:03 +0900

    テーマのディスクリプションを修正。および404ページの作成をした

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+  <main class="page404 pb-10">
+    <section class="inner">
+        <div class="txt-area">
+            <div class="title">
+                <p class="txt-14 en">Not Found</p>
+                <h2 class="txt-24">ページが見つかりません</h2>
+            </div>
+            <div class="txt">
+                <p class="txt-16">
+                    お探しのページは削除されたか、名前が変更された可能性があります。<br>直接アドレスを入力された場合は、アドレスが正しく入力されているかもう一度ご確認ください。
+                </p>
+                <p class="txt-16">お使いのブラウザの再読み込み(リロード)を行ってもこのページが表示される場合は、<a href="<?php echo esc_url(home_url('/')); ?>">トップページ</a>から目的のページをお探しください。</p>
+                <p class="txt-16">
+                  「お問い合わせ」や「web予約」の利用をご希望のお客様は、お手数ですが他の手段をご検討いただけると幸いです。
+                </p>
+                <p class="txt-16">
+                    電話: 090-9227-2013<br>
+                    <a href="https://www.instagram.com/kitas.chiropractic.ise?igsh=MW0zeGxlNGxxeWdreQ==">Instagram</a><br>
+                    <a href="https://lin.ee/1ifxpHN">公式LINE</a>
+                  </p>
+
+            </div>
+        </div>
+    </section>
+  </main>
+<?php get_footer(); ?>
