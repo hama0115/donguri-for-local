e5fe144186ad484ef14c58a91b08300c536282ce
hama0115 <195052720+hama0115@users.noreply.github.com>
2025-12-18 00:44:32 +0900

    メインコンテンツのPHP(HTML)を修正する

--- home.php
+++ home.php
@@ -3,14 +3,15 @@
     <main class="blog_page pb-10">
         <div class="blog_content_wrapper inner flex">
             <div class="blog_wrapper">
-                <div class="archive-title">
-                  <h1>ブログ</h1>
-                  <p>blog</p>
+                <div class="archive_title">
+                  <p class="txt-14">blog</p>
+                  <h1 class="txt-24">ブログ</h1>
                 </div>
-                <div class="archive-desc">駆け出しの猟師としても活動する「カイロプラクティックどんぐり」管理人のブログです。主に猟師の活動、伊賀市などについて発信していきます。</div>
+                <div class="archive_desc txt-16">駆け出しの猟師としても活動する「カイロプラクティックどんぐり」管理人のブログです。主に猟師の活動、伊賀市などについて発信していきます。</div>
                 <?php if(have_posts()): ?>
+                    <section class="article_lists">
                     <?php while(have_posts()): the_post(); ?>
-                        <section class="card_list">
+                        <article class="article_card">
                             <a href="<?php the_permalink(); ?>" class="card">
                                 <div class="card_pic">
                                     <?php if(has_post_thumbnail()): ?>
@@ -24,11 +25,12 @@
                                     <h2 class="card_title"><?php the_title(); ?></h2>
                                 </div>
                             </a>
-                        </section>
+                        </article>
                     <?php endwhile; ?>
-                    <?php else: ?>
-                        <p>記事がありません</p>
-                    <?php endif; ?>
+                    </section>
+                <?php else: ?>
+                    <p>記事がありません</p>
+                <?php endif; ?>
             </div>
             <!-- サイドバーのテンプレートの読み込み -->
             <?php get_sidebar(); ?>
