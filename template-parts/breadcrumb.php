4843fd24ff39af375a57736aac86c0ef70ad8383
hama0115 <195052720+hama0115@users.noreply.github.com>
2026-04-30 15:36:07 +0900

    fix:シンタックスエラーを修正。およびパンくずリストの表示を確認。

--- template-parts/breadcrumb.php
+++ template-parts/breadcrumb.php
@@ -3,7 +3,7 @@
 <?php if (function_exists('bcn_display')): ?>
   <div class="breadcrumb">
     <div class="breadcrumb_inner">
-      <?php bcn_display(): ?>
+      <?php bcn_display(); ?>
     </div>
   </div>
 <?php endif; ?>
